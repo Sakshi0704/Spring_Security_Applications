7e93f84fe70a70faaa61d1c08780dd7290e6391b
Sakshi0704 <banichoudhary0704@gmail.com>
2023-09-20 22:00:18 -0800

    updated spring-security

--- SpringSecurityDBApp2/src/main/java/com/springsecuritydb/SpringSecurityDbApp2Application.java
+++ SpringSecurityDBApp2/src/main/java/com/springsecuritydb/SpringSecurityDbApp2Application.java
@@ -11,12 +11,18 @@ public class SpringSecurityDbApp2Application {
 	}
 
 }
-
 /*
  * In this application I have provided the cutome UserDetails interface implementation 
  * 
  * UserDetails (Interface) ----> CustomerUserDetails (Implementation)(provided by us) ( created this one in this application )
  * 
  * UserDetails (Interface) ---> User (Implementation) (provided in Spring Security) 
+ * 
+ * Also Used Swagger dependance and to Test and RESTApi documnetation we use to create Swagger
+ *  if we want to use Swagger on RestApi which also using spring security then we have to so some configuration also
+ *  first - permit the all end point related to swagger
+ *  second - use annotation in the @SpringBootApplication have to done some configuration 
+ *  like @OpenAPIDefinition (for using the open api) and SecuritySchemes and so on 
+ *  which give the idea about project
+ *  
  * */
- 
