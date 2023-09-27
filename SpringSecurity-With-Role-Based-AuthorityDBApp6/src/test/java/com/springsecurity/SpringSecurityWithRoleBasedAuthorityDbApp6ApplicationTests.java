3542acfb1cf65ba0b40b11a29825b5551dde898a
Sakshi0704 <banichoudhary0704@gmail.com>
2023-09-26 21:38:21 -0800

    created Role based authorities

---
+++ SpringSecurity-With-Role-Based-AuthorityDBApp6/src/test/java/com/springsecurity/SpringSecurityWithRoleBasedAuthorityDbApp6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityWithRoleBasedAuthorityDbApp6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
