8cfd2d2606ed4b762050b4a99c82f756f1a2a841
Sakshi0704 <banichoudhary0704@gmail.com>
2023-09-26 23:05:38 -0800

    update spring security role base authentication

---
+++ SpringSecurity-With-Role-Based-AuthorityDBApp6/src/main/java/com/springsecurity/configuration/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.springsecurity.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+	
+	@Bean
+	public SecurityFilterChain springSecurityConfiguration(HttpSecurity http) throws Exception{
+		
+		http.authorizeHttpRequests(auth -> {
+			auth
+			  .requestMatchers(HttpMethod.POST, "/customers").permitAll()
+			  .requestMatchers(HttpMethod.GET,"/customers","/hello").hasRole("ADMIN")
+			  .requestMatchers(HttpMethod.GET ,"/customers/**").hasAnyRole("ADMIN","USER") 
+			  // what is ** is showing PathVeriable -----
+			  .anyRequest().authenticated();
+		})
+		.csrf(csrf -> csrf.disable())
+		.formLogin(Customizer.withDefaults())
+		.httpBasic(Customizer.withDefaults());
+		
+		return http.build();
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder(); 
+		// this is one of the hashing technique and recommendated one
+	}
+	
+}
