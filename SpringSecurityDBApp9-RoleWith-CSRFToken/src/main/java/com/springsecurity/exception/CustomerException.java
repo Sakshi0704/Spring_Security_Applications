555f73fcba3cd04f824930ae02631e193a5049d9
Sakshi0704 <banichoudhary0704@gmail.com>
2023-09-28 11:58:28 -0800

    developed the app based on csrfToken genrated

---
+++ SpringSecurityDBApp9-RoleWith-CSRFToken/src/main/java/com/springsecurity/exception/CustomerException.java
@@ -0,0 +1,14 @@
+package com.springsecurity.exception;
+
+
+public class CustomerException extends Exception {
+
+	public CustomerException() {
+		super();
+	}
+	
+	public CustomerException(String message) {
+		super(message);
+	}
+	
+}
