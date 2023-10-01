39e7e268a89b28e91417da55713eefe10d39dfad
Sakshi0704 <banichoudhary0704@gmail.com>
2023-09-30 21:32:36 -0800

    updated the application with csrf-Token

--- SpringSecurityDBApp9-RoleWith-CSRFToken/src/main/java/com/springsecurity/controller/CustomerController.java
+++ SpringSecurityDBApp9-RoleWith-CSRFToken/src/main/java/com/springsecurity/controller/CustomerController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +21,7 @@ import com.springsecurity.model.Customer;
 import com.springsecurity.service.CustomerService;
 
 @RestController
+@CrossOrigin("*")
 //@RequestMapping("/jpa")
 public class CustomerController {
 
